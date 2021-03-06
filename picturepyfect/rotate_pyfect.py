Fix tests for rotation function.  96% coverage.

picturepyfect/rotate_pyfect.py
@@ -71,6 +71,12 @@ def rotate_pyfect(image, n_rot=1):
         # Stack rotated matrices to create rotated image
         new_img = np.dstack(list(channel_dict.values()))
     else:
-        new_img = np.array([row[::-1] for row in zip(*image)])
+        while n_rot > 0:
+            image = np.array([row[::-1] for row in zip(*image)])
+
+            # Update number of rotations left
+            n_rot = n_rot - 1
+
+        new_img = image
 
     return new_img

tests/test_rotation_pyfect.py
@@ -5,11 +5,16 @@ import pytest
 # Generate random images for testing
 np.random.seed(42)
 test_image = np.random.rand(3, 4, 5)
-bad_image_1 = np.random.rand(2, 2)
+test_image_2d = np.random.rand(2, 2)
 test_image_1 = rotate_pyfect(test_image, 1)
 test_image_2 = rotate_pyfect(test_image, 2)
 test_image_3 = rotate_pyfect(test_image, 3)
 test_image_4 = rotate_pyfect(test_image, 4)
+
+test_image_2d_1 = rotate_pyfect(test_image_2d, 1)
+test_image_2d_2 = rotate_pyfect(test_image_2d, 2)
+test_image_2d_3 = rotate_pyfect(test_image_2d, 3)
+test_image_2d_4 = rotate_pyfect(test_image_2d, 4)
 
 # test error handling
 def test_error():
@@ -25,8 +30,6 @@ def test_error():
         rotate_pyfect(test_image, 5)
     with pytest.raises(TypeError):
         rotate_pyfect(test_image, "1")
-    with pytest.raises(TypeError):
-        rotate_pyfect(bad_image_1, 1)
 
 
 # test rotation_pyfect returned dimensions
@@ -140,3 +143,8 @@ def test_rows_cols():
     assert (test_image[0, :, 2] == test_image_2[-1, :, 2][::-1]).all()
     assert (test_image[0, :, 2] == test_image_3[:, 0, 2][::-1]).all()
     assert (test_image[0, :, 2] == test_image_4[0, :, 2]).all()
+
+    assert (test_image_2d[0, :] == test_image_2d_1[:, -1]).all()
+    assert (test_image_2d[0, :] == test_image_2d_2[-1, :][::-1]).all()
+    assert (test_image_2d[0, :] == test_image_2d_3[:, 0][::-1]).all()
+    assert (test_image_2d[0, :] == test_image_2d_4[0, :]).all()