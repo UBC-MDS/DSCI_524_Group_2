Fix the dimensions for proper image rotation

picturepyfect/rotate_pyfect.py
@@ -20,30 +20,45 @@ def rotate_pyfect(image, n_rot=1):
     Returns:
     ---------
     rotated_image: numpy.ndarray
-        A n*n or n*n*3 numpy array which is the input image rotated by theta degrees
+        A n*n or n*n*3 numpy array which is the input image rotated by a multiple of 90 degrees
 
     Examples
     --------
-    >>> np.random.seed(2021)
-    >>> image = np.random.rand(2, 2)
-    >>> rotate_pyfect(image, deg=90)
-    array([[ 0.73336936, -0.60597828],
-       [ 0.31267308, -0.13894716]])
+    >>> np.random.seed(42)
+    >>> image = np.random.rand(2, 2, 1)
+    >>> rotate_pyfect(image, deg=1)
+    array([[[0.73199394],
+        [0.37454012]],
+
+       [[0.59865848],
+        [0.95071431]]])
     """
 
     # error handling for invalid type
     if type(image) != np.ndarray:
         raise TypeError("Invalid Type: Image must be a numpy array")
 
+    # error handling for invalid n_rot
+    if type(n_rot) != int:
+        raise TypeError("Invalid Type: n_rot must be integer")
+
+    # error handling for invalid n_rot
+    if n_rot not in set([1, 2, 3, 4]):
+        raise TypeError("Invalid Type: n_rot must be between 1 and 4")
+
+    # error handling for invalid dimensions
+    if len(image.shape) != 3:
+        raise TypeError("Invalid Type: Image must by 3 dimensional")
+
     # Initialize dictionary with each channel being mapped to  it's channel number
     channel_dict = {
-        channel: image[channel, :, :] for channel in range(0, image.shape[0])
+        channel: image[:, :, channel] for channel in range(0, image.shape[2])
     }
 
     # While rotations to do
     while n_rot > 0:
         # For each channel
-        for channel in range(0, image.shape[0]):
+        for channel in range(0, image.shape[2]):
             # Pull matrix to rotate
             rot_mat = channel_dict[channel]
             # rotate the matrix
@@ -53,6 +68,6 @@ def rotate_pyfect(image, n_rot=1):
         n_rot = n_rot - 1
 
     # Stack rotated matrices to create rotated image
-    new_img = np.stack(list(channel_dict.values()))
+    new_img = np.dstack(list(channel_dict.values()))
 
     return new_img